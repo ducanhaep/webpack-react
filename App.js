18aa5fadec3182a8f65501136b51a6a189cbf5b5
ducanhaep <tranducanh1012@gmail.com>
2018-11-19 18:18:39 +0700

    19/11/2018

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import './src/components/navbar';
-import Box from "./src/components/navbar";
+// import Box from "./src/components/navbar";
+import AA from "./testxexport";
 class Lopne extends React.Component {
     render() {
         return <h1>He Nho tao la Lopne</h1>;
@@ -65,13 +65,15 @@ class ducanh extends abc  {
     
 }
 
+
 class TestState extends React.Component{
     constructor(props){
         super(props);
         this.state = {value: 'tran duc anh'};
         this.state = {number: 0};
+        // this.handleChange = this.handleChange.bind(this)
     }
-    handleChange(e){
+    handleChange(e) {
         this.setState({value: e.target.value });
     }
     clickAlert(){
@@ -89,7 +91,7 @@ class TestState extends React.Component{
                 <span>{this.state.value}</span>
                 <br />
                 <input
-                    onChange={this.handleChange.bind(this)}
+                    onChange={(e) => this.handleChange(e)}
                 />
                 <button onClick={this.clickAlert.bind(this)}>
                     Click Here
@@ -99,6 +101,7 @@ class TestState extends React.Component{
                 <button onClick={this.decreaseValue.bind(this)}>-</button>
                 <input value = {this.state.number}/>
                 <button onClick={this.increaseValue.bind(this)}>+</button>
+                <AA />
             </React.Fragment>
         );
     }
@@ -112,6 +115,164 @@ class TestState extends React.Component{
 //     </React.Fragment>
     
 // );
+
+//////////////////////////////////////////////////////////////////////
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+class UserGreeting extends React.Component {
+    render() {
+        return <h1>Welcome back!</h1>;
+    }
+}
+class GuestGreeting extends React.Component {
+    render() {
+        return <h1>Please sign up</h1>;
+    }
+}
+class LoginButton extends React.Component {
+    render() {
+        return (
+            <button onClick={this.props.cc}>
+                Login
+            </button>
+        )
+    }
+}
+class LogoutButton extends React.Component {
+    render() {
+        return (
+            <button onClick={this.props.cc}>
+                Lotout
+            </button>
+        )
+    }
+}
+
+class LoginControl extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleLoginClick = this.handleLoginClick.bind(this);
+      this.handleLogoutClick = this.handleLogoutClick.bind(this);
+      this.state = {isLoggedIn: false};
+    }
+  
+    handleLoginClick() {
+      this.setState({isLoggedIn: true});
+    }
+  
+    handleLogoutClick() {
+      this.setState({isLoggedIn: false});
+    }
+  
+    render() {
+      const isLoggedIn = this.state.isLoggedIn;
+      let button;
+  
+      if (isLoggedIn) {
+        button = <LogoutButton cc={this.handleLogoutClick} />;
+      } else {
+        button = <LoginButton cc={this.handleLoginClick} />;
+      }
+  
+      return (
+        <div>
+            <Greeting isLoggedIn={isLoggedIn} />
+            {isLoggedIn ?
+            (<LoginButton cc= {this.handleLoginClick}/>)
+            :
+            (<LogoutButton cc={this.handleLogoutClick}/>)
+            }
+        </div>
+      );
+    }
+}
+/////////////////////////////////////////////////////////////////////////////
+
+class WarningBanner extends React.Component {
+    render() {
+        if(!this.props.warn) {
+            return null;
+        }
+        return(
+            <div className="warning">
+                Warning!
+            </div>
+        )
+    }
+}
+// function WarningBanner(props) {
+//     if(!props.warn) {
+//         return null;
+//     }
+//     return (
+//         <div className="warning">
+//             Warning!
+//         </div>
+//     );
+// }
+
+class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showWarning: true};
+        this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+    handleToggleClick() {
+        // this.setState((cc) => ({
+        //     showWarning: !cc.showWarning
+        // }));
+        // this.setState({showWarning: !this.state.showWarning})
+        this.setState(function(cc){
+            return {
+                showWarning: !cc.showWarning
+            }
+        })
+        // this.setState(function(){
+        //     return {
+        //         showWarning: !this.state.showWarning
+        //     }
+        // })
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <WarningBanner warn={this.state.showWarning} />
+                <button onClick={this.handleToggleClick}>
+                {this.state.showWarning ? "Hide" : "Show"}
+                </button>
+            </React.Fragment>
+
+        )
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+function increseScore(state, props) {
+    return {score: state.score + 1};
+}
+class Stateoutside extends React.Component {
+    constructor() {
+        super();
+        this.state = {score: 1};
+    }
+    handleIncreaseScore () {
+        this.setState(increseScore);
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <button onClick = {this.handleIncreaseScore.bind(this)}>
+                    {this.state.score}
+                </button>
+            </React.Fragment>
+        )
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////
 class RenderEveryThing extends React.Component {
     render() {
         return (
@@ -120,11 +281,25 @@ class RenderEveryThing extends React.Component {
                 <Testprop1>
                     eheheheheheheh
                 </Testprop1>
+                <TestState />
+                <br />
+                <br />
+                <LoginControl />
+                <br />
+                <br />
+                <Page />
+                <br />
+                <br />
+                <br />
+                <Stateoutside />
             </React.Fragment>
         );
     }
 }
 ReactDOM.render(
-    <TestState />,
+    // <TestState />,
+    // <LoginControl />,
+    // <Page />,
+    <RenderEveryThing />,
     document.getElementById('root')
 );
---
+++ testxexport.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DD (){
+    return <h1>Hello!, Export-defau;t </h1>
+}
+
+function CC(){
+    return <h1>Hello!, Export-Name </h1>
+}
+
+export default DD
+export { CC }
+
